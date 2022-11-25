fcfca505fb0d6dd306e0587fbbd082e4354a8e9f
roshan786143 <roshanbabu.kng@gmail.com>
2022-11-26 01:09:04 +0530

    routers and filters

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const port = 4444;
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop'); 
+// console.log(app);
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    // res.send('<h1>Pls.Add a Product to your Cart</h1>');
-    res.send('<form action="/product" method="post" autocomplete="on"><label for="product">Enter the Product : </label><input type="text" name="product" required><label for="size">Enter Size of the Product : </label><input type="text" name="cost" required><button>add product</button></form>');
-});
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    res.redirect('/');
-    console.log(req.body);
-});
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome to amazon</h1>');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found.</h1>');
 })
 
 app.listen(port);
