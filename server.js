0add355dd2727c51af617c2a6022f028a15b341e
roshan786143 <roshanbabu.kng@gmail.com>
2022-11-25 23:18:14 +0530

    added a new product

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const port = 4040;
+const port = 4444;
 const app = express();
-app.use((req,res,next)=>{
-    console.log('hello middleware-1.');
-    next(); 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    // res.send('<h1>Pls.Add a Product to your Cart</h1>');
+    res.send('<form action="/product" method="post" autocomplete="on"><label for="product">Enter the Product : </label><input type="text" name="product" required><label for="size">Enter Size of the Product : </label><input type="text" name="cost" required><button>add product</button></form>');
 });
-app.use((req,res,next)=>{
-    console.log('hello middleware-2.');
-    res.send('<h1>Hello Rashmika Mandanna</h1>');
+
+app.post('/product',(req,res,next)=>{
+    res.redirect('/');
+    console.log(req.body);
 });
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Welcome to amazon</h1>');
+})
+
 app.listen(port);
